8f57ba7f3b3ce14657915ed1e56c494489b20f27
Rahmat Hidayat <59741013+rhmt-hidayat@users.noreply.github.com>
2025-02-01 18:28:18 +0700

    update db

--- application/config/database.php
+++ application/config/database.php
@@ -75,9 +75,9 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost:3307',
+	'hostname' => 'localhost',
 	'username' => 'root',
-	'password' => 'com15pazt',
+	'password' => '',
 	'database' => 'db_penjualan',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
